53e8a1ca33f60bf99711ffa5f1ec923acf9d70b3
JeanRibac <jeanribac95@gmail.com>
2020-10-16 11:43:06 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import * as tf from "@tensorflow/tfjs"
+import * as facemesh from "@tensorflow-models/facemesh"
+import Webcam from "react-webcam"
+import { drawMesh } from "./utils"
 
 function App() {
+  const webcamRef = React.useRef(null);
+  const canvasRef = React.useRef(null);
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8
+    })
+    setInterval(() => {
+      detect(net)
+    }, 10);
+  }
+
+  //detect function
+  const detect = async (net) => {
+    if (typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      //get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      //set video width/height
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      //set canvas width/height
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      //make detections
+      const face = await net.estimateFaces(video);
+      // console.log(face);
+      //get canvas context for drawing
+      const context = canvasRef.current.getContext("2d");
+      drawMesh(face, context)
+    }
+  }
+
+  runFacemesh()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: 'absolute',
+          marginTop: 'auto',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          marginTop: 'auto',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }}
+      />
     </div>
   );
 }
